4cf80eab84e3342cf92060defb201ad1111a2171
MartoJuanDev <martojuandev@gmail.com>
2023-11-13 15:05:47 -0300

    subo resoluciin ej 3

---
+++ Clase3/myApp/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ProductDetail from './ProductDetail';
+import './App.css';
+
+function App() {
+  const productData = {
+    name: 'Bicicleta Oxea Riddich',
+    description: 'Bicicleta marca Oxea mod. Riddich. MTB. Rodado 29"',
+    price: 200000.00,
+    sku: 'SKU123',
+    quantity: 10,
+  };
+
+  return (
+    <div className="app">
+      <ProductDetail {...productData} />
+    </div>
+  );
+}
+
+export default App;
